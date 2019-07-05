9e188558a8afaede6e8f4d326b29ec20dc9b59ad
shanshanweifeng <594024380@qq.com>
2019-07-05 16:21:19 +0800

    search-change

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <city-header></city-header>
-    <city-search></city-search>
+    <city-search :cityname="cities"></city-search>
     <city-list :cities="cities" :hot="hotcities" :changeletter="getletter"></city-list>
     <city-alphabet :cities="cities" @letter-change="handletterchange" >
     </city-alphabet>
